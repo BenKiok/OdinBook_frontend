62ad5a7b5658145452c8b4c01e1b4440624a715b
Benedict Kioko <benedictmkioko@gmail.com>
2023-02-02 00:46:58 -0500

    Adapt login function for signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,19 @@ function Login(props) {
   const toggleLoginBool = () => {
     setLoginBool(!loginBool);
   };
-  const fetchLoginData = async (event, string, func) => {
+  const fetchUserData = async (event, string, func) => {
     event.preventDefault();
 
     let username = event.target.parentNode.childNodes[0].value,
         password = event.target.parentNode.childNodes[1].value,
-        userData = await fetch('http://localhost:3001/api/' + string,
+        confirmPassword = (string === 'signup' ? event.target.parentNode.childNodes[2].value : password),
+        userData;
+    
+    if (password !== confirmPassword) {
+      return;
+    }
+
+    userData = await fetch('http://localhost:3001/api/' + string,
       {
         method: 'POST',
         headers: {
@@ -44,7 +51,7 @@ function Login(props) {
           <form>
             <input type='text' placeholder='Username' required/>
             <input type='password' placeholder='Password' required/>
-            <button className='auth-btn' onClick={(e) => fetchLoginData(e, 'login', props.setData)}>Log In</button>
+            <button className='auth-btn' onClick={(e) => fetchUserData(e, 'login', props.setData)}>Log In</button>
           </form>
           <button className='auth-btn fb-auth'>Log in with Facebook</button>
           <button className='form-switch-btn' onClick={toggleLoginBool}>Create new account</button>
@@ -63,7 +70,7 @@ function Login(props) {
             <input type='text' placeholder='Username' required/>
             <input type='password' placeholder='Password' required/>
             <input type='password' placeholder='Confirm password' required/>
-            <button className='auth-btn'>Sign Up</button>
+            <button className='auth-btn' onClick={(e) => fetchUserData(e, 'signup', props.setData)}>Sign Up</button>
           </form>
           <button className='auth-btn fb-auth'>Sign up with Facebook</button>
           <button className='form-switch-btn' onClick={toggleLoginBool}>Already have an account?</button>
