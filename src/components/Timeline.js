42a6de2cae98fb902cf641a6ce08a906ae8aae96
Benedict Kioko <benedictmkioko@gmail.com>
2023-02-12 15:39:26 -0500

    Add post head with return/cancel button for single post view

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import PostForm from './PostForm.js';
 import Post from './Post.js';
 import '../stylesheets/Timeline.css';
+import '../stylesheets/Post.css';
 
 function Timeline(props) {
   const [posts, setPosts] = useState(null);
@@ -14,10 +15,18 @@ function Timeline(props) {
   if (focusedPost) {
     return (
       <div className='timeline'>
-        <h4 onClick={() => {
-          focusPost(null);
-          fetchPosts(props.auth);
-        }}>Return to timeline</h4>
+        <div className='postHead'>
+          <svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='circle' className='svg-inline--fa fa-circle round placeholder' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+            <path fill='currentColor' d='M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z'></path>
+          </svg>
+          <h3>{props.auth.user.username}'s Post</h3>
+          <svg onClick={() => {
+            focusPost(null);
+            fetchPosts(props.auth);
+          }} aria-hidden='true' focusable='false' data-prefix='fas' data-icon='xmark' className='svg-inline--fa fa-xmark icon round' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'>
+            <path fill='currentColor' d='M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z'></path>
+          </svg>
+        </div>
         <Post key={focusedPost._id} post={focusedPost} auth={props.auth} focus={focusPost}/>
       </div>
     )
